b8edb94769283efc557516444e68a45fd15a09a4
Hyeon Kim <simnalamburt@gmail.com>
2016-04-21 23:40:59 +0900

    webpack.config.js 대대적으로 수정
    1.  autoprefixer 도입
    2.  ExtractTextPlugin으로 _bundle.css 추출
    3.  프로덕션모드 지원
    4.  자주 사용할 로더 미리 써놓음

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,23 +1,33 @@
 // @flow
 const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var plugins/*: Array<Object>*/ = [];
-if (process.env.WEBPACK === 'release') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }));
-}
+// Always enabled plugins
+const plugs = [
+  new ExtractTextPlugin('_bundle.css')
+];
+
+// Production only plugins
+const prod = [
+  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
+];
 
 module.exports = {
-  context: `${__dirname}/src`,
   entry: './main.js',
+  context: `${__dirname}/src`,
   output: {
+    filename: '_bundle.js',
     path: `${__dirname}/../server/public/build`,
     publicPath: '/build/',
-    filename: '_bundle.js'
   },
-  devtool: 'source-map',
-  plugins: plugins,
   module: {
     loaders: [
+      { test: /\.txt$/, loader: 'raw' },
+      { test: /\.png$/, loader: 'file?name=static/[hash].[ext]' },
+      { test: /\.(woff2?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file?name=static/[hash].[ext]' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
+      { test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!stylus') },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -25,5 +35,8 @@ module.exports = {
         query: { presets: ['es2015', 'stage-3', 'react'] }
       }
     ]
-  }
+  },
+  plugins: process.env.NODE_ENV !== 'production' ? plugs : plugs.concat(prod),
+  devtool: 'source-map',
+  postcss: ()/*: Array<Object>*/ => [autoprefixer]
 };
