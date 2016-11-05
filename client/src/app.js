aa7d333d8e2ecf7cbd60dbcb196e6f1d5a9cb4ca
Hyeon Kim <simnalamburt@gmail.com>
2016-11-06 02:08:24 +0900

    Apply style

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,13 @@
 // @flow
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './app.styl'
 
 ReactDOM.render(
   <div>
-    <h1>Hello, React!</h1>
-    <p>My own react practice!</p>
+    <h1>OpenIRC</h1>
+    <p>IRC Client for everyone!</p>
   </div>,
   document.getElementById('main')
 );
