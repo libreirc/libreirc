0d86da6f703f7174320e6a9c380f662ceca8d03c
Hyeon Kim <simnalamburt@gmail.com>
2016-11-20 23:35:04 +0900

    Enable static file serving

--- server/src/main.py
+++ server/src/main.py
@@ -3,8 +3,9 @@ from sanic.response import json
 
 app = Sanic(__name__)
 
-@app.route("/")
-async def test(request):
-    return json({"hello": "world"})
+# Static file serving
+# TODO: static file serving need to be used only in debug mode
+app.static('/', './public/index.html')
+app.static('*', './public')
 
 app.run(host="0.0.0.0", port=4321)
