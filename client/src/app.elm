c5223ffee1ef0ffafa0cf9944a2864a5a7e8a9ba
Hyeon Kim <simnalamburt@gmail.com>
2016-11-20 17:55:14 +0900

    <form> 기반으로 수정

--- client/src/app.elm
+++ client/src/app.elm
@@ -2,27 +2,30 @@ module App exposing (..)
 
 import String exposing (dropRight)
 import Html exposing (..)
-import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (id, class, placeholder, value)
+import Html.Events exposing (onSubmit, onInput)
+import Html.Attributes exposing (id, class, type_, placeholder, value)
 
-main = Html.beginnerProgram {
-  model = model,
-  view = view,
-  update = update }
+main =
+  Html.beginnerProgram {
+    model = model,
+    view = view,
+    update = update
+  }
 
 
 -- Model
-type alias Model = {
-  logs : List String,
-  typing : String }
+type alias Model =
+  {
+    logs : List String,
+    typing : String
+  }
 
 model = Model [] ""
 
 
 -- Update
-type Msg
-  = Message
-  | Typing String
+type Msg = Message
+         | Typing String
 
 update : Msg -> Model -> Model
 update msg model = case msg of
@@ -32,12 +35,18 @@ update msg model = case msg of
 
 -- View
 view : Model -> Html Msg
-view model = div [ id "openirc" ] [
-  ul [] (
-    List.map (\elem ->
-      li [] [text elem]
-    ) model.logs
-  ),
-  span [] [ text "김젼" ],
-  input [ value model.typing, placeholder "Hi!", onInput Typing ] [],
-  button [ onClick Message ] [ text "+" ] ]
+view model =
+  div [ id "openirc" ] [
+    ul [] (
+      List.map (\elem ->
+        li [] [text elem]
+      ) model.logs
+    ),
+    form [onSubmit Message] [
+      p [] [
+        label [] [text "김젼"],
+        input [value model.typing, placeholder "Hi!", onInput Typing] [],
+        input [type_ "submit", value "전송"] []
+      ]
+    ]
+  ]
