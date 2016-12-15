d5a224477fa8138bd5ebb482d847ca4f731e35d9
Hyeon Kim <simnalamburt@gmail.com>
2016-12-15 11:20:11 +0900

    Remove compiler warnings
    You can see compiler warnings with the command below
    
        elm-make src/App.elm --warn
    
    Related issue:
      https://github.com/openirc/openirc/issues/30

--- client/src/App.elm
+++ client/src/App.elm
@@ -1,19 +1,12 @@
 module App exposing (..)
 
-import String exposing (isEmpty, startsWith)
 import Html exposing (..)
-import Html.Events exposing (onSubmit, onInput, onClick)
-import Html.Attributes exposing (id, class, type_, placeholder, value, autocomplete)
-import Dict exposing (Dict)
-import Dict as D
-import Tuple exposing (first, second)
-import Task exposing (Task)
-import Dom.Scroll exposing (toBottom)
 import Model exposing (model)
 import View exposing (view)
 import Update exposing (update)
 
 
+main : Program Never Model.Model Update.Msg
 main =
     Html.program
         { init = ( model, Cmd.none )
