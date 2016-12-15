9524b0cac9413068cfe1992a7ba7451165289d6f
Heejong Ahn <heejongahn@gmail.com>
2016-12-15 18:28:32 +0900

    Add comment for View.elm

--- client/src/View.elm
+++ client/src/View.elm
@@ -9,10 +9,24 @@ import Tuple exposing (second)
 import Model exposing (..)
 import Update exposing (Msg(..))
 
+{-| This module includes all view of OpenIRC.
 
--- View
+# Top-level View
 
+@docs view
 
+# bufferListsDiv components
+
+@docs bufferListsDiv, serverNameItem, bufferNameItems, newBufferItem
+
+# currentBufferDiv components
+
+@docs currentBufferDiv, currentBufferInfoDiv, logsList, newLineForm
+-}
+
+{-| Top-level view of OpenIRC. It consists of two components - `bufferListsDiv` showing information about all open
+buffer(sidebar), and `currentBufferDiv` showing information about the current buffer.
+-}
 view : Model -> Html Msg
 view model =
     div [ id "openirc" ]
@@ -21,9 +35,8 @@ view model =
         ]
 
 
-{-|
-    bufferListsDiv : `<div>` containing the lists of buffers, indexed by server (sidebar)
-    TODO: Support multi-server architecture - multiple `<ul>` can exist
+{-| Div showing information of all open buffer, indexed by server (sidebar).
+TODO: Support multi-server architecture - multiple `<ul>` must be able to exist
 -}
 bufferListsDiv : Model -> Html Msg
 bufferListsDiv model =
@@ -33,9 +46,10 @@ bufferListsDiv model =
         ]
 
 
-{-|
-    serverNameItem : `<li>` showing server name.
-    TODO: Make server to be shown when clicked
+{-| List item showing a server name.
+
+    relatedMsgs : List Msg
+    relatedMsgs == [ ChangeBuffer ]
 -}
 serverNameItem : ServerName -> Html Msg
 serverNameItem name =
@@ -48,9 +62,11 @@ serverNameItem name =
         li [ class "buffer-item server-name" ] [ serverNameSpan name ]
 
 
-{-|
-    bufferNameItems : [ `<li>` showing buffer (channel) name ]
-    TODO: Add server name to arguement, filter namePairs which does not belong to the server
+{-| `List` of list items, where each of them showing the name of a open buffer.
+TODO: Add server name to arguement, only show namePairs which belongs to the server.
+
+    relatedMsgs : List Msg
+    relatedMsgs == [ ChangeBuffer, CloseBuffer ]
 -}
 bufferNameItems : Model -> List (Html Msg)
 bufferNameItems model =
@@ -73,22 +89,21 @@ bufferNameItems model =
             (\namePair ->
                 li
                     [ class <| itemClass namePair ]
-                    [ bufferNameSpan namePair
-                    , closeAnchor namePair
-                    ]
+                    [ bufferNameSpan namePair , closeAnchor namePair ]
             )
     in
         List.map render (D.keys model.bufferMap)
 
 
-{-|
-    newBufferItem : `<li>` for new buffer join
+{-| List item for joining new buffer.
+
+    relatedMsgs : List Msg
+    relatedMsgs == [ CreateBuffer, TypeNewChannelName ]
 -}
 newBufferItem : Model -> Html Msg
 newBufferItem model =
     let
-        newChannelName =
-            getNewChannelName model model.currentServerName
+        newChannelName = getNewChannelName model model.currentServerName
     in
         li [ class "buffer-item new-buffer" ]
             [ form [ id "new-buffer-form", onSubmit <| CreateBuffer model.currentServerName ]
@@ -105,8 +120,7 @@ newBufferItem model =
             ]
 
 
-{-|
-    currentBufferDiv : `<div>` containing currently selected buffer, including logs and new line form
+{-| Div showing information about the current buffer, including logs and new line form.
 -}
 currentBufferDiv : Model -> Html Msg
 currentBufferDiv model =
@@ -117,30 +131,25 @@ currentBufferDiv model =
         ]
 
 
-{-|
-    currentBufferInfoDiv model
+{-| Div showing information, that is, server name and channel name, for the current buffer.
 -}
 currentBufferInfoDiv : Model -> Html Msg
 currentBufferInfoDiv model =
     let
-        currentPositionText =
-            model.currentServerName ++ " | " ++ model.currentChannelName
+        currentPositionText = model.currentServerName ++ " | " ++ model.currentChannelName
     in
         div [ id "current-buffer-info" ]
             [ text currentPositionText ]
 
 
-{-|
-    logsList : `<ul>` showing current buffer's logs
+{-| List showing the current buffer's logs.
 -}
 logsList : Model -> Html Msg
 logsList model =
     let
-        currentNamePair =
-            ( model.currentServerName, model.currentChannelName )
+        currentNamePair = ( model.currentServerName, model.currentChannelName )
 
-        currentBuffer =
-            getBuffer model currentNamePair
+        currentBuffer = getBuffer model currentNamePair
     in
         ul [ id "logs" ]
             (currentBuffer.lines
@@ -148,20 +157,19 @@ logsList model =
             )
 
 
-{-|
-    newLineForm : `<form>` for typing new line for current buffer
+{-| Form for typing new line for the current buffer.
+
+    relatedMsgs : List Msg
+    relatedMsgs == [ SendLine, TypeNewLine ]
 -}
 newLineForm : Model -> Html Msg
 newLineForm model =
     let
-        currentNamePair =
-            ( model.currentServerName, model.currentChannelName )
+        currentNamePair = ( model.currentServerName, model.currentChannelName )
 
-        currentBuffer =
-            getBuffer model currentNamePair
+        currentBuffer = getBuffer model currentNamePair
 
-        currentNick =
-            getNick model model.currentServerName
+        currentNick = getNick model model.currentServerName
     in
         form [ id "new-line-form", onSubmit SendLine ]
             [ label [ id "new-line-label" ] [ text currentNick ]
