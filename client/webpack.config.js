dea329b44269dba9d68ae3b00486956f5f9dd536
Hyeon Kim <simnalamburt@users.noreply.github.com>
2017-01-06 18:06:07 +0900

    Update webpack.config.js
    WTF

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,7 @@ const plugins = [
 ];
 
 // Production only plugins
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   plugins.concat([
     // Pass the 'NODE_ENV=production' environment variable to the child processes.
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
